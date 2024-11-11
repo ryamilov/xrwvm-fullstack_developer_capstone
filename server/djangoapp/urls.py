7a3a88f02a44ead85637743d202f72c8634f1637
Roman Yamilov <roman.yamilov@gmail.com>
2024-11-11 11:18:55 -0500

    add models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     # path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,10 +31,12 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
-    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
     'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
-    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
