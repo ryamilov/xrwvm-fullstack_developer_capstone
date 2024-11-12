6a03f1d14a69abc33319ed130665cf3a2ec91f78
Roman Yamilov <roman.yamilov@gmail.com>
2024-11-12 07:18:53 -0500

    dockerize

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,19 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    ('https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0',
-        '.proxy.cognitiveclass.ai/'),
-    ('https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0',
-        '.proxy.cognitiveclass.ai'),
-    ('https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01',
-        '.proxy.cognitiveclass.ai')]
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    ('https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0',
-        '.proxy.cognitiveclass.ai/'),
-    ('https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0',
-        '.proxy.cognitiveclass.ai'),
-    ('https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01',
-        '.proxy.cognitiveclass.ai')]
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
+    'https://romanyamilov-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://romanyamilov-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -107,8 +101,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        ('django.contrib.auth.password_validation',
-         '.UserAttributeSimilarityValidator'),
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
